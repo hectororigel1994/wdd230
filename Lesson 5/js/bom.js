668571945c8bfad0c9a386ad56a3013aa177e726
hectororigel1994 <horigel@byui.edu>
2022-02-03 19:30:47 -0600

    Commit

--- Lesson 5/js/bom.js
+++ Lesson 5/js/bom.js
@@ -1,15 +1,21 @@
 function compute(){
 
+	
     var text = document.getElementById('favchap').value;
     
+        
+            
       var x = document.createElement("LI");
       var t = document.createTextNode(text);
+        
         var btn = document.createElement("button");
         
         btn.innerHTML = "x";
         btn.style.color = "Red";
         btn.setAttribute("id", "btn");
         btn.setAttribute("class", "btn");
+            document.getElementById("myBtn").addEventListener("click", function() {
       x.appendChild(t);
      x.appendChild(btn); document.getElementById("div1").appendChild(x);
+            });
         }
