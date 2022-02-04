65b099eea71b6d3f68a57eac635889439a90f1b8
hectororigel1994 <horigel@byui.edu>
2022-02-03 19:43:55 -0600

    Commit

--- Lesson 5/js/bom.js
+++ Lesson 5/js/bom.js
@@ -14,8 +14,14 @@ function compute(){
         btn.style.color = "Red";
         btn.setAttribute("id", "btn");
         btn.setAttribute("class", "btn");
+        btn.setAttribute("onclick", "removeDummy()");
             document.getElementById("myBtn").addEventListener("click", function() {
       x.appendChild(t);
      x.appendChild(btn); document.getElementById("div1").appendChild(x);
             });
         }
+    
+    function removeDummy() {
+     var elem = document.getElementById('div1');
+     elem.parentNode.removeChild(elem);
+    }
